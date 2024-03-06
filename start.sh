f88efdb4bf012d62a6bcc2abec9b24673a768b19
loandre <uloandre@gmail.com>
2024-03-06 11:07:00 -0300

    in progress

--- start.sh
+++ start.sh
@@ -3,10 +3,10 @@
 # Caminho do arquivo APK no container
 APK_PATH="/home/androidusr/testIDVouTeHomologar.apk"
 
-# Argumento para o script run_test.sh (ajuste conforme necessário)
-TEST_SCRIPT_ARG="hml704appsPro"
+# Argumento para o script run_test.sh
+#TEST_SCRIPT_ARG="hml704appsPro"
 
-# Espera até que o ADB esteja pronto e o emulador seja carregado
+# Esperar até que o ADB esteja pronto e o emulador seja carregado
 echo "Esperando o ADB estar pronto e o emulador carregar..."
 until adb devices | grep 'device$'; do
     echo "Aguardando o emulador..."
@@ -14,7 +14,7 @@ until adb devices | grep 'device$'; do
 done
 
 echo "Emulador carregado e pronto."
-sleep 20  # Espera adicional para garantir a estabilidade do emulador
+sleep 20 
 
 echo "Instalando APK..."
 adb install $APK_PATH
